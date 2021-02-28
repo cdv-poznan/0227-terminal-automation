eb0477aa724ebf5960bc9653265333ef4206bb13
Adrian Juszczak <adrian.juszczak@cdv.pl>
2021-02-28 09:10:06 +0100

    Dynamic webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -2,47 +2,57 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
-const config = {
-  entry: {
-    main: ['./src/main', './src/style.scss'],
-  },
-  output: {
-    path: path.resolve(__dirname, './dist'),
-    // filename: ''
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        use: [
-          {
-            loader: 'babel-loader',
-          },
-        ],
-      },
-      {
-        test: /\.scss$/,
-        use: [
-          MiniCssExtractPlugin.loader,
-          // {
-          //   loader: 'style-loader',
-          // },
-          {
-            loader: 'css-loader',
-          },
-          {
-            loader: 'sass-loader',
-          },
-        ],
-      },
+const config = (env, args) => {
+  console.log('env', env);
+  console.log('args', args);
+
+  const PRODUCTION = args.mode === 'production';
+
+  return {
+    entry: {
+      main: ['./src/main', './src/style.scss'],
+    },
+    output: {
+      path: path.resolve(__dirname, './dist'),
+      // filename: ''
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          use: [
+            {
+              loader: 'babel-loader',
+            },
+          ],
+        },
+        {
+          test: /\.scss$/,
+          use: [
+            PRODUCTION
+              ? MiniCssExtractPlugin.loader
+              : {
+                  loader: 'style-loader',
+                },
+            {
+              loader: 'css-loader',
+            },
+            {
+              loader: 'sass-loader',
+            },
+          ],
+        },
+      ],
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: './src/index.html',
+      }),
+      new MiniCssExtractPlugin({
+        filename: 'style.css',
+      }),
     ],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './src/index.html',
-    }),
-    new MiniCssExtractPlugin(),
-  ],
+  };
 };
 
 module.exports = config;
