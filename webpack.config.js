3d7d06096792083673fde420373057549b1d98eb
Adrian Juszczak <adrian.juszczak@cdv.pl>
2021-02-28 09:01:40 +0100

    Styles

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,11 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 const config = {
-  entry: ['./src/main'],
+  entry: {
+    main: ['./src/main', './src/style.scss'],
+  },
   output: {
     path: path.resolve(__dirname, './dist'),
     // filename: ''
@@ -17,12 +20,28 @@ const config = {
           },
         ],
       },
+      {
+        test: /\.scss$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          // {
+          //   loader: 'style-loader',
+          // },
+          {
+            loader: 'css-loader',
+          },
+          {
+            loader: 'sass-loader',
+          },
+        ],
+      },
     ],
   },
   plugins: [
     new HtmlWebpackPlugin({
       template: './src/index.html',
     }),
+    new MiniCssExtractPlugin(),
   ],
 };
 
